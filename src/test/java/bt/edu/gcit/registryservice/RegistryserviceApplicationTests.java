aa72d79df2c8c8908b3c3301d10c3b26d2572290
Lungten <12230028.gcit@rub.edu.bt>
2026-03-19 10:55:49 +0000

    Initial commit

---
+++ src/test/java/bt/edu/gcit/registryservice/RegistryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package bt.edu.gcit.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
